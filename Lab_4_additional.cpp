9a3ce8cf3baea287b9ece2c9720aeec74efe73f8
Vlad Suvorov <rabbsuira@gmail.com>
2024-05-29 16:58:02 +0300

    And some more

---
+++ Lab_4_additional.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T> T SafeInput(const string& inputMessage, const string& errorMessage)
+{
+	T tempValue;
+
+	while (true)
+	{
+		cout << inputMessage << endl;
+
+		if (!(cin >> tempValue) || (cin.peek() != '\n'))
+		{
+			cout << errorMessage << endl << "Restarting process" << endl << endl;
+			cin.ignore(1000, '\n');
+			cin.clear();
+			continue;
+		}
+		return tempValue;
+	}
+};
+
+
+int main()
+{
+	double array[100];
+
+	int usableAmount = SafeInput<int>("Input array work space", "Value should be type of Integer");
+
+	for (int i = 0; i < usableAmount; i++)
+	{
+		//array[i] = SafeInput<double>("Input array element " + to_string(i), "Value should be number");
+		array[i] = i;
+	}
+
+	int counter = 0;
+
+	for (int i = 0; i < usableAmount; i++)
+	{
+		//counter++;
+		
+		//if (counter % 3 != 0) continue;
+
+		if (i != 0 && i % 2 == 0)
+		{
+			for (int j = i; j < usableAmount; j++)
+			{
+				array[j] = array[j + 1];
+			}
+			//counter = 0;
+			//i--;
+			usableAmount--;
+		}
+
+
+
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < usableAmount; i++)
+	{
+		cout << array[i] << endl;
+	}
+}
+
---
+++ Lab_4_main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	double array[] = { 6, 14, 151, 3, 5, 2, 5, 6, 2.12, 4.2 };
+
+	double sum = 0;
+	int maxIndex = 0;
+	int minIndex = 0;
+
+	//cout << array[78] << endl;
+	//cout << sizeof(array) / sizeof(array[0]) << endl;
+
+	for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+	{
+		minIndex = array[i] < array[minIndex] ? i : minIndex;
+		maxIndex = array[i] > array[maxIndex] ? i : maxIndex;
+	}
+
+	for (int i = (minIndex < maxIndex ? minIndex : maxIndex) + 1; i < (minIndex > maxIndex ? minIndex : maxIndex); i++)
+	{
+		sum += array[i];
+	}
+
+	cout << sum << endl;
+
+	return 0;
+}
+
